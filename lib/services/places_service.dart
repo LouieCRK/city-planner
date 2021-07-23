1c8de6083395eccd5292c9cb63e075ef51079af3
Louie Crook <55953362+LouieCrook@users.noreply.github.com>
2021-07-23 22:05:43 +0100

    Update places_service.dart

--- lib/services/places_service.dart
+++ lib/services/places_service.dart
@@ -1,15 +1,15 @@
+import 'package:http/http.dart';
 import 'package:uk_city_planner/models/nightlife/NightlifeModel.dart';
 
 class HttpService {
-  final String postUrl =
+  final Uri postUrl =
       "https://maps.googleapis.com/maps/api/place/nearbysearch/json?""location=51.3550048,-2.3744071&"
-      "radius=25000&type=nightlife&keyword=nightclub%20bar%20drinks%20music&key=AIzaSyDqtrPbqvNbhfEtb273GQb4obuRd4-AUuo";
+      "radius=25000&type=nightlife&keyword=nightclub%20bar%20drinks%20music&key=AIzaSyDqtrPbqvNbhfEtb273GQb4obuRd4-AUuo" as Uri;
 
-  Future<List<NightlifeModel>> getPosts()
-  asyc
-  {
-  Response res = await
-  get(postUrl);
-}
+  Future<List<NightlifeModel>> getPosts() async {
+    Response res = await get(postUrl);
+    print(res);
+
+  }
 }
 
