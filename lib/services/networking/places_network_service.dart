60f0ac9691f546326bc09be63d8936ab0671d7a8
Louie Crook <55953362+LouieCrook@users.noreply.github.com>
2021-07-27 14:50:17 +0100

    Update places_network_service.dart
    modifying api request to get better results

--- lib/services/networking/places_network_service.dart
+++ lib/services/networking/places_network_service.dart
@@ -5,13 +5,13 @@ import 'package:uk_city_planner/models/places_model.dart';
 class PlacesNetworkService {
   final key =
       'AIzaSyDqtrPbqvNbhfEtb273GQb4obuRd4-AUuo'; // google places api key - used within url as $key
-  var radius = 22000; // just over 9 miles radius
+  var radius = 7500; // just over 4.6 miles radius
 
   Future<List<Result>> findRestaurants(String latitude,
       String longitude) async {
     String url =
         'https://maps.googleapis.com/maps/api/place/nearbysearch/json?'
-        'location=$latitude,$longitude&radius=$radius&type=%20cafe%20point_of_interest&keyword=food&key=$key&rankby=prominence';
+        'location=$latitude,$longitude&radius=$radius&type=restaurant%20point_of_interest&keyword=food%20dinner%20dining&key=$key&rankby=prominence';
     final response = await get(
       Uri.parse(url),
     );
